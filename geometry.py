f5a10cd6b35f2dc20f09c05193e6b96324d269b1
coin50 <chu285@purdue.edu>
2025-01-21 22:21:46 -0500

    inline

--- geometry.py
+++ geometry.py
@@ -1,24 +1,16 @@
 EPS = 1e-8
 class Point:
-	def __init__(self,x,y):
-		self.x = x; self.y = y
-	def __add__(self,P):
-		return Point(self.x+P.x, self.y+P.y)
-	def __sub__(self,P):
-		return Point(self.x-P.x, self.y-P.y)
-	def __mul__(self,k):
-		return Point(self.x*k, self.y*k)
-	def __matmul__(self,P):
-		return self.x*P.x + self.y*P.y
-	def __xor__(self,P):
-		return self.x*P.y - self.y*P.x
-	def dis2(self):
-		return self.x**2 + self.y**2
+	def __init__(self,x,y): self.x = x; self.y = y;
+	def __add__(self,P): 	return Point(self.x+P.x, self.y+P.y)
+	def __sub__(self,P): 	return Point(self.x-P.x, self.y-P.y)
+	def __mul__(self,k): 	return Point(self.x*k, self.y*k)
+	def __matmul__(self,P): return self.x*P.x + self.y*P.y
+	def __xor__(self,P): 	return self.x*P.y - self.y*P.x
+	def __repr__(self): 	return f"({self.x}, {self.y})"
+	def dis2(self): 		return self.x**2 + self.y**2
 	def unit(self):
 		d = self.dis2()
 		return self*(d**-0.5) if d else Point(0,0)
-	def __repr__(self):
-		return f"({self.x}, {self.y})"
 
 def area(pts):
 	n = len(pts)
