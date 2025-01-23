641134fc548b91c807f2f1b0612004c548128c13
coin50 <chu285@purdue.edu>
2025-01-23 13:54:50 -0500

    Convex hull

--- geometry.py
+++ geometry.py
@@ -8,9 +8,7 @@ class Point:
 	def __xor__(self,P): 	return self.x*P.y - self.y*P.x
 	def __repr__(self): 	return f"({self.x}, {self.y})"
 	def dis2(self): 		return self.x**2 + self.y**2
-	def unit(self):
-		d = self.dis2()
-		return self*(d**-0.5) if d else Point(0,0)
+	def unit(self):			return self * (self.dis2()**-0.5)
 
 def area(pts):
 	n = len(pts)
@@ -25,6 +23,21 @@ def intersect(L1,L2):
 	k = ((C-A)^(D-C)) / ((B-A)^(D-C))
 	return A + (B-A)*k
 
+def convex_hull(pts):
+	if not pts: return []
+	if all( (p-pts[0]).dis2() < EPS for p in pts ): return [pts[0]]
+	pts = sorted(pts, key=lambda p:(p.x,p.y))
+	low = []
+	for p in pts:
+		while len(low) > 1 and (low[-1]-low[-2])^(p-low[-1]) < EPS:
+			low.pop()
+		low.append(p)
+	high = []	
+	for p in reversed(pts):
+		while len(high) > 1 and (high[-1]-high[-2])^(p-high[-1]) < EPS:
+			high.pop()
+		high.append(p)
+	return low + high[1:-1]
 """
 a = Point(1,1)
 b = Point(3,3)
