7c35be9b73ae4491a04837c9578c9539d340debc
coin50 <chu285@purdue.edu>
2025-02-02 00:06:08 -0500

    shorten

--- hungarian.py
+++ hungarian.py
@@ -1,40 +1,36 @@
 def hungarian(g):
-    BIG = 1<<55
+    BIG = 1<<31
     n = len(g)
-    pot = [-BIG]*(2*n)
+    pot = [BIG]*(2*n)
     match = [-1]*(2*n)
  
-    for _ in range(n):
-        s = match.index(-1)
+    for s in range(n):
         par = [-1]*n
         v = [1]*n
  
         min_slack = [(g[s][i]-pot[s]-pot[i+n], s) for i in range(n)]
         while True:
-            cap, L, R = BIG,-1,-1
+            cap,L,R = BIG,-1,-1
             for i,(x,y) in enumerate(min_slack): 
-                if not v[i]: continue
-                if x < cap: cap,L,R = x,y,i
+                if v[i] and x < cap: cap,L,R = x,y,i
  
             pot[s] += cap
             for i in range(n):
                 if not v[i]:    pot[i+n] -= cap
                 if par[i] > -1: pot[i] += cap
             
-            if match[R+n] == -1: 
-                while L > -1:
-                    match[R+n] = L
-                    R, match[L] = match[L], R
-                    L = par[L]
-                break
-            
-            M = match[R+n]
+            if (M := match[R+n]) == -1: break
             par[M] = L
             v[R] = 0
             for i,(x,y) in enumerate(min_slack):
-                if not v[i]: continue
-                slack = g[M][i]-pot[M]-pot[i+n]
-                min_slack[i] = (slack,M) if slack < x-cap else (x-cap,y)
+                if v[i]:
+                    slack = g[M][i]-pot[M]-pot[i+n]
+                    min_slack[i] = (slack,M) if slack < x-cap else (x-cap,y)
+        
+        while L > -1:
+            match[R+n] = L
+            R, match[L] = match[L], R
+            L = par[L]
     return sum(pot), match[:n]
 
 """
