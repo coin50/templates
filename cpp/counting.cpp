82d298b3e7907e2466a59186fb9ccb2b05ca7d04
coin50 <chu285@purdue.edu>
2025-12-22 21:42:37 -0600

    Update counting.cpp

--- cpp/counting.cpp
+++ cpp/counting.cpp
@@ -1,23 +1,25 @@
-#define ll long long
-#define MOD 0x3b800001
-#define pow p0w
+const ll MOD = 998244353;
 ll p0w(ll x,ll e){
-    ll acc = x;
-    ll p = 1;
-    while(e){
-        if(e&1) p = p*acc%MOD;
-        acc = acc*acc%MOD;
-        e >>= 1;
-    }
-    return p;
+	ll p = 1;
+	ll acc = x;
+	while(e){
+		if(e&1) p = p*acc%MOD;
+		acc = acc*acc%MOD;
+		e /= 2;
+	}
+	return p;
 }
 
-#define MAX 100
-ll fact[MAX+1];
-ll anti[MAX+1];
-void init_fact(){
-    fact[0] = 1;
-    for( int i = 1; i <= MAX; i++ ) fact[i] = fact[i-1]*i%MOD;
-    anti[MAX] = pow(fact[MAX],MOD-2);
-    for( int i = MAX-1; i >= 0; i-- ) anti[i] = anti[i+1]*(i+1)%MOD;
-}
+const int MAX = 1e6 + 1;
+const array<ll,MAX> fact = []{
+	array<ll,MAX> fact = {1};
+	rep(i,1,MAX) fact[i] = fact[i-1]*i%MOD;
+	return fact;
+}();
+const array<ll,MAX> anti = []{
+	array<ll,MAX> anti;
+	anti[MAX-1] = p0w(fact[MAX-1],MOD-2);
+	for( int i = MAX-2; i >= 0; i-- ) anti[i] = anti[i+1]*(i+1)%MOD;
+	return anti;
+}();
+ll cmb(ll n, ll k){ return 0 <= k and k <= n ? fact[n]*anti[k]%MOD*anti[n-k]%MOD : 0; }
